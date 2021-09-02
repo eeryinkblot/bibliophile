87aa2aeea7df296e51cc165770e7be362f40a7f2
eeryinkblot <mrcluedude@hotmail.de>
2021-09-02 20:30:39 +0200

    👌 IMPROVE: Add improvements for readability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -16,30 +18,26 @@ public class Main {
     public static void main(String[] args) {
         WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
         ChromeOptions chromeOptions = new ChromeOptions();
-        // chromeOptions.setHeadless(true);
+        chromeOptions.setHeadless(true);
         WebDriver driver = new ChromeDriver(chromeOptions);
 
         driver.get("https://stadtbibliothek.magdeburg.de/Mein-Konto");
-        WebElement user = driver.findElement(By.name("dnn$ctr362$Login$Login_COP$txtUsername"));
-        user.sendKeys("%USER%");
+        fillUsername(driver, "73$9814002");
+        fillPassword(driver, "31.05.1988");
+        login(driver);
 
-        WebElement pass = driver.findElement(By.name("dnn$ctr362$Login$Login_COP$txtPassword"));
-        pass.sendKeys("%PASSWORD%");
+        getHeuteAbgeben(driver).forEach(System.out::println);
 
-        WebElement login = driver.findElement(By.name("dnn$ctr362$Login$Login_COP$cmdLogin"));
-        login.click();
+    }
 
-        // Warten bis Login durch ist
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
-        // Hole die Tabelle
-        WebElement table = driver.findElement(By.className("oclc-patronaccount-checkboxtable"));
-        List<WebElement> rows = table.findElements(By.tagName("tr"));
-        rows.remove(0); // Entferne "Alle ausgewählt"
-
+    @NotNull
+    private static List<TableRow> getHeuteAbgeben(WebDriver driver) {
         final int TITEL = 2;
         final int MEDIUM = 4;
         final int ABGABEDATUM = 6;
+        // Warten bis Login durch ist
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        List<WebElement> rows = getRows(driver);
         List<TableRow> tableRows = rows.stream()
                 .map(row -> row.findElements(By.tagName("td")))
                 .map(row -> new TableRow(
@@ -54,11 +52,32 @@ public class Main {
             String[] dateParts = row.getAbgabedatum().split("\\.");
 
             LocalDate date = LocalDate.of(Integer.valueOf(dateParts[2]), Integer.valueOf(dateParts[1]), Integer.valueOf(dateParts[0]));
-            return date.isEqual(LocalDate.now());
+            return date.isEqual(LocalDate.now().plus(6, ChronoUnit.DAYS));
         }).collect(Collectors.toList());
+        return heuteAbgeben;
+    }
 
-        heuteAbgeben.forEach(System.out::println);
+    private static void fillUsername(WebDriver driver, String username) {
+        WebElement user = driver.findElement(By.name("dnn$ctr362$Login$Login_COP$txtUsername"));
+        user.sendKeys(username);
+    }
 
+    private static void fillPassword(WebDriver driver, String password) {
+        WebElement pass = driver.findElement(By.name("dnn$ctr362$Login$Login_COP$txtPassword"));
+        pass.sendKeys(password);
+    }
+
+    private static void login(WebDriver driver) {
+        WebElement login = driver.findElement(By.name("dnn$ctr362$Login$Login_COP$cmdLogin"));
+        login.click();
+    }
+
+    @NotNull
+    private static List<WebElement> getRows(WebDriver driver) {
+        WebElement table = driver.findElement(By.className("oclc-patronaccount-checkboxtable"));
+        List<WebElement> rows = table.findElements(By.tagName("tr"));
+        rows.remove(0); // Entferne "Alle ausgewählt"
+        return rows;
     }
 
 }
